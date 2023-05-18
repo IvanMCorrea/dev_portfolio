9f752e0b3310a942f18ae3df6f74044b52cc67a3
Ivan Correa <ivaanmc95@gmail.com>
2023-05-18 17:21:20 -0300

    Add menu render in sidebar and appbar

--- app/_components/AppBar.tsx
+++ app/_components/AppBar.tsx
@@ -1,15 +1,30 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import { ThemeToggle } from "./ThemeToggle";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
 function AppBar() {
-  const [expanded, setExpanded] = useState<Boolean>(false);
-
+  const menu = [{ id: "Home", link: "#", icon: faHome }];
   return (
     <section
-      className={`fixed flex justify-center items-center bottom-5 w-5/6 h-14 px-5 bg-neutral-300 dark:bg-neutral-800 appbar`}
+      className={`fixed flex justify-center items-center appbar bottom-5 w-screen`}
     >
-      <ThemeToggle expanded={expanded} />
+      <div className="flex justify-center items-center px-5 bg-neutral-300 dark:bg-neutral-800 w-5/6 h-14 rounded-3xl">
+        <ThemeToggle expanded={false} />
+        {menu &&
+          menu[0] &&
+          menu.map((item) => (
+            <Link href={item.link} key={item.id}>
+              <FontAwesomeIcon
+                icon={item.icon}
+                className="text-neutral-500 dark:text-neutral-100"
+                size="lg"
+              />
+            </Link>
+          ))}
+      </div>
     </section>
   );
 }
--- app/_components/Sidebar.tsx
+++ app/_components/Sidebar.tsx
@@ -2,16 +2,32 @@
 import React, { useState } from "react";
 import { ThemeToggle } from "./ThemeToggle";
 import { SidebarToggle } from "./SidebarToggle";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
 function Sidebar() {
   const [expanded, setExpanded] = useState<Boolean>(false);
   const sidebarWidth = expanded ? "w-64" : "w-18";
-
+  const menu = [{ id: "Home", link: "#", icon: faHome }];
   return (
     <aside
       className={`fixed flex left-0 top-0 h-screen p-5 flex-col ${sidebarWidth} bg-neutral-300 dark:bg-neutral-800 items-start sidebar`}
     >
       <SidebarToggle expanded={expanded} setExpanded={setExpanded} />
+      {menu &&
+        menu[0] &&
+        menu.map((item) => (
+          <Link href={item.link} key={item.id}>
+            <FontAwesomeIcon
+              icon={item.icon}
+              className="text-neutral-500 dark:text-neutral-100"
+              size="lg"
+            />
+            {expanded && <span className="pl-3">{item.id}</span>}
+          </Link>
+        ))}
+
       <ThemeToggle expanded={expanded} />
     </aside>
   );
