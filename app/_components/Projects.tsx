eb637a09aca0279ea7b18124212b5d5039ce0394
Ivan Correa <ivaanmc95@gmail.com>
2023-05-21 22:12:10 -0300

    Add projects render

--- app/_components/Projects.tsx
+++ app/_components/Projects.tsx
@@ -1,9 +1,53 @@
+import Image from "next/image";
 import React from "react";
 
 type Props = {};
 
 const Projects = (props: Props) => {
-  return <div>Projects</div>;
+  const projects = [
+    {
+      title: "Arcane",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      image: "/prog.svg",
+      destacado: true,
+    },
+    {
+      title: "Arcane",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      image: "/prog.svg",
+    },
+    {
+      title: "Arcane",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      image: "/prog.svg",
+    },
+  ];
+  return (
+    <section>
+      <div className="grid grid-cols-3 gap-5">
+        {projects[0] &&
+          projects.map(
+            (item) =>
+              !item.destacado && (
+                <article>
+                  <Image
+                    src={item.image}
+                    alt="Working Developer"
+                    width={100}
+                    height={100}
+                    priority
+                  />
+                  <h3>{item.title}</h3>
+                  <p>{item.description}</p>
+                </article>
+              )
+          )}
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import AboutMe from "./_components/AboutMe";
 import Landing from "./_components/Landing";
+import Projects from "./_components/Projects";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center m-auto px-12 md:px-24 py-12 max-w-5xl gap-20">
       <Landing />
       <AboutMe />
+      <Projects />
     </main>
   );
 }
